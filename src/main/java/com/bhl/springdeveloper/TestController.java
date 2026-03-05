3f2c8ff947172340f1cc9cfb5c86f1400d7da24e
inho001 <qogydka@naver.com>
2026-03-05 12:25:05 +0900

    26년 3월 12일

---
+++ src/main/java/com/bhl/springdeveloper/TestController.java
@@ -0,0 +1,21 @@
+package com.bhl.springdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // @Controller + @ResponseBody
+// 모든 메서드가 데이터를 리턴한다
+// @Controller -> 기본적으로 리턴하는 것이 HTML 파일 이름이다
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers() {
+        return testService.getAllMembers();
+    }
+}
---
+++ src/main/java/com/bhl/springdeveloper/TestService.java
@@ -0,0 +1,18 @@
+package com.bhl.springdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    private TestRepository testRepository;
+
+    public List<Member> getAllMembers() {
+        return testRepository.findAll();
+    }
+
+}
