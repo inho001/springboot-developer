a5bf15c300a06b077809a2ccdac0d4f3abb9cdc7
inho001 <qogydka@naver.com>
2026-05-19 16:51:57 +0900

    fix: controller fix

--- src/main/java/com/bhl/springdeveloper/controller/BlogApiController.java
+++ src/main/java/com/bhl/springdeveloper/controller/BlogApiController.java
@@ -1,21 +1,20 @@
 package com.bhl.springdeveloper.controller;
 
-import com.bhl.springdeveloper.dto.AddArticleRequest;
+import lombok.RequiredArgsConstructor;
 import com.bhl.springdeveloper.dao.Article;
+import com.bhl.springdeveloper.dto.AddArticleRequest;
 import com.bhl.springdeveloper.dto.ArticleResponse;
 import com.bhl.springdeveloper.dto.UpdateArticleRequest;
 import com.bhl.springdeveloper.service.BlogService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class BlogApiController {
-
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
@@ -27,31 +26,33 @@ public class BlogApiController {
 
     @GetMapping("/api/articles")
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
-        List<Article> articles = blogService.findAll();
-        List<ArticleResponse> result = articles.stream()
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
                 .map(ArticleResponse::new)
                 .toList();
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok()
+                .body(articles);
     }
 
     @GetMapping("/api/articles/{id}")
     public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
         Article article = blogService.findById(id);
-        return ResponseEntity.ok().body(new ArticleResponse(article));
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
         blogService.delete(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .build();
     }
 
     @PutMapping("/api/articles/{id}")
-    public ResponseEntity<Article> updateArticle(
-            @RequestBody UpdateArticleRequest request, @PathVariable long id) {
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
         Article updatedArticle = blogService.update(id, request);
-        return ResponseEntity.ok().body(updatedArticle);
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
-
-
 }
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -3,7 +3,7 @@ if (modifyBtn) {
     const params = new URLSearchParams(location.search);
     const id = params.get("id")
     modifyBtn.addEventListener('click', () => {
-        fetch(`api/articles/${id}`, {
+        fetch(`/articles/${id}`, {
             method: 'PUT',
             headers: {
                 "Content-Type": "application/json"
@@ -15,6 +15,28 @@ if (modifyBtn) {
         }).then(() => {
             alert('수정이 완료되었습니다');
             location.replace(`/articles/${id}`);
+        });
+    })
+}
+
+const createButton = document.getElementById('create-btn');
+if (createButton) {
+    createButton.addEventListener('click', event => {
+    // 서버에 블로그 글 등록 요청 보내기
+    // 등록할 글 제목, 내용을 요청 바디에 포함시켜야지
+    // 등록이 완료되면 '등록 완료되었습니다' 라는 팝업창 띄우고, 그 다음에 글 목록보기 요청을 서버에 보내기
+        fetch('/articles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        }).then(() => {
+            alert('등록 완료되었습니다');
+            location.replace('/articles')
         });
     })
 }
